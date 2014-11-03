bfed4083f4cad882cc90df986341825d77966275
Miguel Quintal <mquintal88@gmail.com>
2014-11-03 17:41:27 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# grunt-merge-copy v0.0.4
+
+> Merge two directories.
+
+
+
+## Getting Started
+This plugin requires Grunt `~0.4.0`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-merge-copy --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-merge-copy');
+```
+
+
+
+## Copy task
+_Run this task with the `grunt merge-copy` command._
+
+Task targets, files and options may be specified according to the grunt [Configuring tasks](http://gruntjs.com/configuring-tasks) guide.
+### Options
+
+#### common
+Type: `String`
+Default: `common`
+
+This option represents common folder name. 
+
+#### specific
+Type: `String`
+Default: `specific`
+
+This option represents specific folder name. 
+
+#### encoding
+Type: `String`  
+Default: `grunt.file.defaultEncoding`
+
+The file encoding to copy files with.
+
+### Usage Examples
+
+Assuming you have following project structure:
+
+...
+
+```js
+ 'merge-copy': {
+	options: {
+		common: 'base'
+		specific: 'clientx'
+	},
+	main: {
+		file: [ { src: 'app', dest: 'dist' } ]
+	}
+ }
+```
---
+++ tasks/merge-copy.js
@@ -0,0 +1,175 @@
+module.exports = function( grunt ) {
+    'use strict';
+	
+    var fs = require( 'fs' )
+      , path = require( 'path' )
+    
+    grunt.registerMultiTask( 'merge-copy', 'Merge two directories', function() {
+        var options = {
+			common: this.options().common || 'common'
+		  , specific: this.options().common || 'specific'
+		  , encoding: this.options().common || grunt.file.defaultEncoding
+		};
+		
+		var copyOptions = {
+			encoding: options.encoding,
+		};
+		
+		
+         
+        /**
+         * Method responsible for returning files into given path
+         * 
+         * @param {String} basePath - path to list all files.
+         * 
+         * @date 2014-10-03
+         * */  
+        function getPathFiles( basePath ) {
+            var arrFiles = [];
+             
+            // Check if base path is a directory.
+            if( grunt.file.isDir( basePath ) ) {
+                
+                grunt.file.recurse( basePath, function( file  ) {
+                    arrFiles.push( file );
+                });
+                
+            } else {
+                grunt.log.errorlns( 'Directory "', path, '" NOT FOUND.' );
+            }
+            
+            return arrFiles;
+        }
+        
+        
+        /**
+         * Returns all common and specific files.
+         *
+         * @date 2014-11-03
+         * */
+        function getFiles( commonDir, specificDir ) {
+            var arrCommonFiles = []
+              , arrClientFiles = [];
+            
+            return { 
+                common: getPathFiles( commonDir )
+              , specific: getPathFiles( specificDir )
+            };
+        }
+        
+        /**
+         * Returns all common and specific files.
+         * 
+         * @param {String} basePath
+         * @param {String} file
+         * 
+         * @date 2014-11-03
+         * */
+        function getRelativePath( basePath, file ) {
+            var absolutePath = path.resolve( file, '.' )
+              , relativePath = absolutePath.replace( basePath, '' );
+            
+            relativePath = relativePath.split( path.sep );
+            
+            if( relativePath[ 0 ] === '' ) {
+                relativePath.shift( );
+            }
+            relativePath.shift( );
+            
+            return relativePath.join( path.sep );
+        }
+        
+        /**
+         * Merges common and specific array.
+         *
+         * 
+         * @date 2014-11-03
+         * @author arodrigues
+         * */
+        function mergeArrays( basePath ,commonFiles, specificFiles ) {
+            var commonPath = ''
+              , specificPath = ''
+              , arrResult = []
+              , fileFound = false;//JSON.parse( JSON.stringify( commonFiles ) );
+            
+            basePath = path.resolve( basePath, '.' );
+            
+            for( var common in commonFiles ) {
+                if( commonFiles.hasOwnProperty( common ) ) {
+                    fileFound = false;
+                    commonPath = getRelativePath( basePath, commonFiles[ common ] );
+                    
+                    if( specificFiles ) {
+                        
+                        specificFiles.forEach( function( file ) {
+                            specificPath = getRelativePath( basePath, file );
+                            
+                            // Check if relative and common are equal. 
+                            if( specificPath === commonPath ) {
+                                // Delete this index from result array.
+                                arrResult.push( { relative: specificPath, absolute: file } );
+                                fileFound = true;
+                                return ;
+                            }
+                            
+                        });
+                        
+                    }
+                    
+                    if( !fileFound ) {
+                        arrResult.push( { relative: commonPath, absolute: commonFiles[ common ] } );
+                    }
+                    
+                }
+            }
+            
+            return arrResult;
+        }
+        
+        /**
+         * Copy all file to dest directory.
+         * 
+         * @param {String} dest - destination directory
+         * @param {Array} files - all files to copy [{ relative: '', absolute: '' }, ...]
+         *
+         * @date 2014-11-03
+         * @author arodrigues
+         * */
+        function copyFiles( dest, files ) {
+            dest = path.resolve( dest, '.' );
+            
+            
+            files.forEach( function( file ) {
+                var src = path.resolve( file.absolute, '.' )
+                  , destFile = path.resolve( dest + '/' + file.relative, '.' );
+                
+                // Copy file.  
+                grunt.file.copy( src, destFile, copyOptions );
+                
+                grunt.log.debug( 'File copied: \n\tfrom: ', src, '\n\tto: ' + destFile );
+            });    
+        }
+        
+        
+        this.files.forEach( function( filePair ) {
+            var commonDir = filePair.src[0] + '/' + options.common
+              , specificDir = filePair.src[0] + '/' + options.specific
+              , files = null; 
+              
+            
+            files = getFiles( commonDir, specificDir );
+            
+            grunt.log.debug( 'Common Files: \n\t', files.common );
+            grunt.log.debug( 'Specific Files: \n\t', files.specific, '\n' );
+            
+            // Merge commoin files
+            files = mergeArrays( filePair.src[0], files.common, files.specific );
+            
+            grunt.log.debug( 'Merged Files: \n\t', files );
+            
+             // Copy all files
+            copyFiles( filePair.dest, files );
+        });
+    });
+
+};
