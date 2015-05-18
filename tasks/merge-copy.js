d267cb533e2a855041f03cdbc1339a82ee80fd25
QuintalM <miguel.quintal@blip.pt>
2015-05-18 23:24:16 +0100

    Organize code to lib folder to be testable

--- tasks/merge-copy.js
+++ tasks/merge-copy.js
@@ -1,30 +1,23 @@
-var path = require( 'path' );
-var mergeCollectFiles = require('node-merge-collect-files');
-
 module.exports = function( grunt ) {
     'use strict';
+	
+	var mergeCopy = require('../lib/merge-copy')(grunt);
 
     grunt.registerMultiTask( 'merge-copy', 'Merge two or more directories', function() {
-        var options = this.options();
-
-        if ( !options.encoding ) {
-            options.encoding = grunt.file.defaultEncoding;
-        }
-
-        var copyOptions = {
-            encoding: options.encoding
-        };
-
-
-        var files = mergeCollectFiles( options.directories, options.patterns, options.globOptions );
-
-        for ( var relativeFilePath in files ) {
-            if ( files.hasOwnProperty( relativeFilePath ) ) {
-                var fileInfo = files[ relativeFilePath ];
-
-                grunt.file.copy( fileInfo.absolutePath, path.join( options.destination, relativeFilePath ), copyOptions );
-            }
-        }
+        var options = this.options(),
+			files = [];
+		
+		// Assuming default encoding.
+		options.encoding = options.encoding || grunt.file.defaultEncoding;
+		
+		// Init module with current task options. 
+		mergeCopy = mergeCopy(options);
+		
+		// Get all merged files
+		files = mergeCopy.files();
+		
+		// Copy all merged file to the destination.
+		mergeCopy.copy(files || []);
     });
 
 };
