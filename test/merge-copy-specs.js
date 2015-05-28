a98102a4840c21b26d94d8f5025f9365f16062c5
mquintal <mquintal88@gmail.com>
2015-05-28 22:52:51 +0100

    Improve code implementation and create a Class to manage files data.

--- test/merge-copy-specs.js
+++ test/merge-copy-specs.js
@@ -1,6 +1,7 @@
 describe('merge-copy tests', function() {
 	var grunt = require('grunt');
 	var fs = require('fs');
+	var path = require('path');
 	var expect = require('chai').expect;
 	var mergeCopy = require('../lib/merge-copy')(grunt);
 	var merge;
@@ -22,26 +23,29 @@ describe('merge-copy tests', function() {
 		
 		it('should merge files form two directories', function() {
 			var files = merge.files();
+			var indexPath = path.normalize(TARGET_DIR + 'index.html');
+			var somefilePath = path.normalize(TARGET_DIR + 'somefolder/somefile.html');
 			
-			expect(files).to.have.a.property('index.html');
-			expect(files).to.have.a.property('user-list.html');
-			expect(files).to.have.a.property('somefolder/somefile.html');
+			expect(files).is.a('Array');
+			expect(files.length).to.equal(3);
+			expect(files[0].getTarget()).to.be.equal(indexPath);
+			expect(files[1].getTarget()).to.be.equal(somefilePath);
 		});
 		
 		it('should merge files and copy them (A->B)', function() {
 			var files = merge.files();
+			var indexPath = TARGET_DIR + 'index.html';
+			var somefilePath = TARGET_DIR + 'somefolder/somefile.html';
 			var indexFileContent = '';
 			var somefileContent = '';
 			
 			merge.copy(files);
 			
-			indexFileContent = fs.readFileSync(TARGET_DIR + 'index.html');
-			somefileContent =  fs.readFileSync(TARGET_DIR + 'somefolder/somefile.html');
+			indexFileContent = fs.readFileSync(indexPath);
+			somefileContent =  fs.readFileSync(somefilePath);
 			
-			expect(files).to.have.a.property('index.html');
-			expect(files).to.have.a.property('user-list.html');
-			expect(grunt.file.exists(TARGET_DIR + 'index.html')).to.be.true;
-			expect(grunt.file.exists(TARGET_DIR + 'user-list.html')).to.be.true;
+			expect(grunt.file.exists(indexPath)).to.be.true;
+			expect(grunt.file.exists(somefilePath)).to.be.true;
 			expect(indexFileContent.toString()).to.contains('clientB');
 			expect(somefileContent.toString()).to.contains('clientB');
 		});
@@ -49,19 +53,19 @@ describe('merge-copy tests', function() {
 		it('should merge files and copy them (B->A)', function() {
 			mockOtions.directories = ['test/mockProject/app/clientB', 'test/mockProject/app/clientA'];
 			var merge = mergeCopy(mockOtions);
+			var indexPath = path.normalize(TARGET_DIR + 'index.html');
+			var somefilePath = path.normalize(TARGET_DIR + 'somefolder/somefile.html');
 			var files = merge.files();
 			var indexFileContent = '';
 			var somefileContent = '';
 			
 			merge.copy(files);
 			
-			indexFileContent = fs.readFileSync(TARGET_DIR + 'index.html');
-			somefileContent =  fs.readFileSync(TARGET_DIR + 'somefolder/somefile.html');
+			indexFileContent = fs.readFileSync(indexPath);
+			somefileContent =  fs.readFileSync(somefilePath);
 			
-			expect(files).to.have.a.property('index.html');
-			expect(files).to.have.a.property('user-list.html');
-			expect(grunt.file.exists(TARGET_DIR + 'index.html')).to.be.true;
-			expect(grunt.file.exists(TARGET_DIR + 'user-list.html')).to.be.true;
+			expect(grunt.file.exists(indexPath)).to.be.true;
+			expect(grunt.file.exists(somefilePath)).to.be.true;
 			expect(indexFileContent.toString()).to.contains('clientA');
 			expect(somefileContent.toString()).to.contains('clientA');
 		});
@@ -83,10 +87,13 @@ describe('merge-copy tests', function() {
 		
 		it('should merge files form two directories', function() {
 			var files = merge.files();
+			var indexPath = path.normalize(TARGET_DIR + 'index.html');
+			var userListPath = path.normalize(TARGET_DIR + 'user-list.html');
 			
-			expect(files).to.have.a.property('index.html');
-			expect(files).to.have.a.property('user-list.html');
-			expect(files).not.to.have.a.property('somefolder/somefile.html');
+			expect(files).is.a('Array');
+			expect(files.length).to.equal(2);
+			expect(files[0].getTarget()).to.be.equal(indexPath);
+			expect(files[1].getTarget()).to.be.equal(userListPath);
 		});
 		
 	});
@@ -106,10 +113,13 @@ describe('merge-copy tests', function() {
 		
 		it('should merge files form two directories', function() {
 			var files = merge.files();
+			var indexPath = path.normalize(TARGET_DIR + 'index.html');
+			var somefilePath = path.normalize(TARGET_DIR + 'somefolder/somefile.html');
 			
-			expect(files).to.have.a.property('index.html');
-			expect(files).not.to.have.a.property('user-list.html');
-			expect(files).to.have.a.property('somefolder/somefile.html');
+			expect(files).is.a('Array');
+			expect(files.length).to.equal(2);
+			expect(files[0].getTarget()).to.be.equal(indexPath);
+			expect(files[1].getTarget()).to.be.equal(somefilePath);
 		});
 		
 	});
